d2b19057b75e6de1b045d252f0eab0406804a446
Niyog V <77136963+niyogv@users.noreply.github.com>
2023-06-16 15:57:40 +0530

    latest

--- test_signup.py
+++ test_signup.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
 
-username="user@16j"
-password='Test@123'
+username=""
+password=''
 
 class Test_sign_up:
 
     @pytest.fixture()
     def test_invoke(self):
         self.driver = webdriver.Chrome()
-        self.driver.get('https://qadashboard.iome.ai/')
+        self.driver.get('https:www.example.com')
         self.driver.maximize_window()
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//button').click()
@@ -22,7 +22,7 @@ class Test_sign_up:
         self.driver.find_element(By.LINK_TEXT, 'Create new.').click()
         time.sleep(2)
         check=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div/div/div[2]/article')
-        assert check.text=='Create a new account', 'sign up page is broken'
+        assert check.text=='Create a new account', 'sign up page is broken'  #Checks for the validation and throws error
         self.driver.find_element(By.XPATH, '//input[@placeholder="Pick a username (5 or more characters)"]').send_keys(username)
         self.driver.find_element(By.XPATH, '//input[@type="password"]').send_keys(password)
         self.driver.find_element(By.XPATH, '//input[@type="checkbox"]').click()
@@ -32,26 +32,26 @@ class Test_sign_up:
 
         #srp page
         check1=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div/div/div[6]/button/span')
-        assert check1.text=='Create secret recovery phrase for me', 'srp page is broken'
+        assert check1.text=='Create secret recovery phrase for me', 'srp page is broken'  #Checks for the validation and throws error
         self.driver.find_element(By.XPATH, '//button').click()
         time.sleep(3)
 
         #cp srp page
         check2=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div/div/a/div/article')
-        assert check2.text=="I'll do this later", 'copy srp page is broken'
+        assert check2.text=="I'll do this later", 'copy srp page is broken'  #Checks for the validation and throws error
         self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div/div/a/div/article').click()
         time.sleep(2)
 
         #skip srp page
         check3=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div/div/div/div[2]/article')
-        assert check3.text=='Skip saving your secret recovery phrase?', 'Skipping srp page is broken'
+        assert check3.text=='Skip saving your secret recovery phrase?', 'Skipping srp page is broken' #Checks for the validation and throws error
         self.driver.find_element(By.XPATH, '//input').click()
         self.driver.find_element(By.XPATH, '//button[2]').click()
         time.sleep(6)
 
         #TOS page
         check4=self.driver.find_element(By.TAG_NAME, 'button')
-        assert check4.text=='Create IOMe account', 'Tos page is broken'
+        assert check4.text=='Create IOMe account', 'Tos page is broken' #Checks for the validation and throws error
         self.driver.find_element(By.XPATH, '//input').click()
         self.driver.find_element(By.XPATH, '//button').click()
         time.sleep(10)
