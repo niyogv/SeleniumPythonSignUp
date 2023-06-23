a5113684258b2185bf6de5d0f502cbb957399cba
Niyog V <77136963+niyogv@users.noreply.github.com>
2023-06-23 15:24:53 +0530

    Update test_signup.py

--- test_signup.py
+++ test_signup.py
@@ -4,13 +4,16 @@ import pytest
 import random
 import string
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 
 
 class Test_sign_up:
 
     @pytest.fixture()
     def test_invoke(self):
-        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        chrome_options=Options
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('')
         self.driver.maximize_window()
         time.sleep(1)
