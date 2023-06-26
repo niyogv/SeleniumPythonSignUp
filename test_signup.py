626b3205719cdef8925ba1b540f6052fbb55ca9b
Niyog V <77136963+niyogv@users.noreply.github.com>
2023-06-26 17:57:02 +0530

    Update test_signup.py

--- test_signup.py
+++ test_signup.py
@@ -25,7 +25,8 @@ class Test_sign_up:
     def test_createnew(self,test_invoke):
         username = ''.join(random.choices(string.ascii_lowercase, k=5))  #Generates the random username with lenght 5
         password_characters = string.ascii_letters + string.digits + string.punctuation #Generates the password with length 10 with special, number, letters
-        password = ''.join(random.choices(password_characters, k=10))
+        password_lenght=10
+        password = ''.join(random.sample(password_characters, password_length))
         time.sleep(1)
         print(username)
         print(password)
